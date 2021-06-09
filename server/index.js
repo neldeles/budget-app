064ebb6954745e8b68171c65d250755642e1550c
neldeles <neldeles@gmail.com>
2021-06-09 15:51:11 +0800

    added dashboard route and authorization middleware

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ app.use(cors());
 // register and login routes
 app.use("/auth", require("./routes/jwtAuth"));
 
+// dashboard route
+app.use("/dashboard", require("./routes/dashboard"));
+
 app.listen(5000, () => {
   console.log("server is running on port 5000");
 });
